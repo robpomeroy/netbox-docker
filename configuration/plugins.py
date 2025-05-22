52c4a9efb2bec34a3fa9e22552c19e71c9f0d6bd
Rob Pomeroy <robpomeroy@users.noreply.github.com>
2025-05-22 11:58:43 +0100

    Diode plugin

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,10 +4,19 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-# PLUGINS = ["netbox_bgp"]
+PLUGINS = [
+    "netbox_diode_plugin",
+]
 
-# PLUGINS_CONFIG = {
-#   "netbox_bgp": {
-#     ADD YOUR SETTINGS HERE
-#   }
-# }
+PLUGINS_CONFIG = {
+    "netbox_diode_plugin": {
+        # Diode gRPC target for communication with Diode server
+        "diode_target_override": "grpc://localhost:8081/diode",
+
+        # Username associated with changes applied via plugin
+        "diode_username": "diode",
+
+        # netbox-to-diode client_secret created during diode bootstrap.
+        "netbox_to_diode_client_secret": "uYytVxfDmIb8iUeMreCOfThsx1h7ws8faAfkSthhWWM="
+    },
+}
